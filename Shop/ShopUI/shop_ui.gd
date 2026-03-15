a67177f8fb8fb0d3b096fd17c31587aa48c2f921
Allan Sechrist <sechristallan@gmail.com>
2026-03-15 12:31:37 +0900

    Add shop ui

--- Shop/ShopUI/shop_ui.gd
+++ Shop/ShopUI/shop_ui.gd
@@ -1,2 +1,16 @@
 extends Control
 class_name ShopUI
+
+const ITEM_CARD = preload("res://items/itemUI/item_card.tscn")
+@onready var grid_container: GridContainer = %GridContainer
+
+@export var shop_items: ShopCatalog
+
+func _ready() -> void:
+	populate_shop()
+	
+func populate_shop() -> void:
+	for item in shop_items.items:
+		var card = ITEM_CARD.instantiate()
+		grid_container.add_child(card)
+		card.setup(item)
--- items/itemUI/item_card.gd
+++ items/itemUI/item_card.gd
@@ -4,17 +4,24 @@ class_name ItemCard
 @onready var card_name_label: Label = %CardNameLabel
 @onready var card_icon: TextureRect = %CardIcon
 @onready var perchase_button: Button = %PerchaseButton
+@onready var description_label: Label = %DescriptionLabel
 
 var item: Item
+var amount := 1
 
 func setup(new_item: Item):
 	item = new_item
 	
-	card_icon.texture = item.card_icon
+	card_icon.texture = item.icon
 	card_name_label.text = item.display_name
+	description_label.text = item.description
 	
 	var text := "Buy"
 	for resource in item.costs:
-		text += " %s: %d " % [resource, item.costs[resource]]
+		text += " %d " % [item.costs[resource]]
 		
 	perchase_button.text = text
+
+
+func _on_perchase_button_pressed() -> void:
+	Events.perchase_request.emit(item, amount)
