398fbc12dc4ec7759407a52a3efc4ad5cc6fd5c1
Allan Sechrist <sechristallan@gmail.com>
2026-03-29 13:41:50 +0900

    Add cast timer to ship avatar, add laser effects

--- ShipManager/ship_avatar.gd
+++ ShipManager/ship_avatar.gd
@@ -2,12 +2,14 @@ extends Node2D
 class_name ShipAvatar
 
 #const LASER_EFFECT = preload("uid://bsduwcnx8btvt")
-const LASER_EFFECT = preload("uid://diteqlph40eak")
+#const LASER_EFFECT = preload("uid://diteqlph40eak")
 @export var fire_rate_min: float = 5.0
 @export var fire_rate_max: float = 10.0
-
+@export var cast_length: float = 2.0 # How long should the ship fire the laser.
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var laser_timer: Timer = $LaserTimer
+@onready var cast_timer: Timer = $CastTimer
+@onready var fire_timer: Timer = $FireTimer
+
 @onready var laser_ray_cast: RayCast2D = $LaserRayCast
 
 var target_position: Vector2
@@ -15,9 +17,11 @@ var fire_target_position: Vector2
 
 func _ready() -> void:
 	randomize_fire_timer()
-	laser_timer.timeout.connect(_on_laser_timer_timeout)
-	laser_timer.wait_time = randf_range(fire_rate_min, fire_rate_max)
-	laser_timer.start(randf_range(0.1, laser_timer.wait_time))
+	fire_timer.timeout.connect(_on_fire_timer_timeout)
+	fire_timer.wait_time = randf_range(fire_rate_min, fire_rate_max)
+	fire_timer.start(randf_range(0.1, fire_timer.wait_time))
+	cast_timer.timeout.connect(_on_cast_timer_timeout)
+	cast_timer.wait_time = cast_length
 
 func _process(delta: float) -> void:
 	global_position = global_position.lerp(target_position, 6.0 * delta)
@@ -26,17 +30,22 @@ func set_icon(texture: Texture2D) -> void:
 	sprite_2d.texture = texture
 	
 func randomize_fire_timer() -> void:
-	laser_timer.wait_time = randf_range(fire_rate_min, fire_rate_max)
-	
+	fire_timer.wait_time = randf_range(fire_rate_min, fire_rate_max)
+
 func fire_laser() -> void:
 	print("Ship fired a laser!")
 	var angle_to_target := global_position.angle_to_point(fire_target_position)
 	laser_ray_cast.global_position = global_position
 	laser_ray_cast.global_rotation = angle_to_target
-	laser_ray_cast.set_is_casting()
+	laser_ray_cast.is_casting = true
 
-func _on_laser_timer_timeout() -> void:
+func _on_fire_timer_timeout() -> void:
 	fire_laser()
 	randomize_fire_timer()
-	laser_timer.start()
-	
+	cast_timer.start()
+	print("Fire time out!")
+
+func _on_cast_timer_timeout() -> void:
+	laser_ray_cast.is_casting = false
+	fire_timer.start()
+	print("Caster time out!")
--- assets/effects/laser_ray_cast.gd
+++ assets/effects/laser_ray_cast.gd
@@ -2,9 +2,14 @@ extends RayCast2D
 
 @export var cast_speed := 700.0
 @export var max_length := 1400.0
+@export var start_distance := 10.0
+@export var growth_time := 0.1
 @export var is_casting := false: set = set_is_casting
 @export var color := Color.WHITE: set = set_color
 @onready var line_2d: Line2D = $Line2D
+@onready var line_width := line_2d.width
+
+var tween: Tween = null
 
 func _ready() -> void:
 	set_color(color)
@@ -16,6 +21,12 @@ func _physics_process(delta: float) -> void:
 		max_length,
 		cast_speed * delta
 	)
+	
+	var laser_end_position := target_position
+	force_raycast_update()
+	if is_colliding():
+		laser_end_position = to_local(get_collision_point())
+	line_2d.points[1] = laser_end_position
 
 func set_is_casting(new_value: bool) -> void:
 	if is_casting == new_value:
@@ -24,8 +35,31 @@ func set_is_casting(new_value: bool) -> void:
 	
 	set_physics_process(is_casting)
 	
-	if is_casting == false:
-		target_position.x = 0.0
+	if not line_2d:
+		return
+	
+	if is_casting:
+		var laser_start := Vector2.RIGHT * start_distance
+		line_2d.points[0] = laser_start
+		line_2d.points[1] = laser_start
+		appear()
+	else:
+		target_position = Vector2.ZERO
+		disappear()
+		
+func appear() -> void:
+	line_2d.visible = true
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(line_2d, "width", line_width, growth_time * 2.0).from(0.0)
+	
+func disappear() -> void:
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(line_2d, "width", 0.0, growth_time).from_current()
+	tween.tween_callback(line_2d.hide)
 
 func set_color(new_color: Color) -> void:
 	color = new_color
